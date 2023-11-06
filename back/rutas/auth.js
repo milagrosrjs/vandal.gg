7c54ad1c998cecdf1231a4a1ca1fb0cac36a865a
JuanDiproX <juandidbz@gmail.com>
2023-11-06 11:13:45 -0300

    Fixing login

--- back/rutas/auth.js
+++ back/rutas/auth.js
@@ -1,17 +1,39 @@
-// auth.js
-
 const express = require('express');
 const router = express.Router();
 const connection = require('../bd'); // Importa la conexión a la base de datos
 
+// Middleware para analizar datos del formulario
+router.use(express.urlencoded({ extended: true }));
+
 // Ruta para el registro de usuarios
 router.post('/registro', (req, res) => {
-  // Implementa la lógica de registro aquí
-});
+  // Recibe los datos del formulario
+  const { nombre, email, contrasena } = req.body;
 
-// Ruta para el inicio de sesión de usuarios
-router.post('/inicio-sesion', (req, res) => {
-  // Implementa la lógica de inicio de sesión aquí
+  // Realiza la inserción de datos en la tabla Usuarios
+  connection.query(
+    'INSERT INTO Usuarios (nombre, email, contrasena) VALUES (?, ?, ?)',
+    [nombre, email, contrasena],
+    (error, results) => {
+      if (error) {
+        console.error('Error al insertar datos:', error);
+        res.status(500).json({ error: 'Error al registrar usuario' });
+      } else {
+        console.log('Usuario registrado con éxito');
+
+        // Realiza una consulta SELECT para verificar los datos recién insertados
+        connection.query('SELECT * FROM Usuarios', (selectError, selectResults) => {
+          if (selectError) {
+            console.error('Error al seleccionar datos:', selectError);
+          } else {
+            console.log('Datos de usuarios:', selectResults);
+          }
+        });
+
+        res.status(200).json({ message: 'Usuario registrado con éxito' });
+      }
+    }
+  );
 });
 
 module.exports = router;
