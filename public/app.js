e4c2d4f66e1698313a102d52a054c161b7d9a961
JuanDiproX <146556789+JuanDiproX@users.noreply.github.com>
2023-12-20 17:36:43 -0300

    Test 1 tienda

--- public/app.js
+++ public/app.js
@@ -21,14 +21,19 @@ async function agregarAlCarrito(producto) {
         // Recupera la lista de productos en el carrito desde localStorage (si existe)
         const carrito = JSON.parse(localStorage.getItem(`carrito_${user.uid}`)) || [];
 
-        // Agrega el producto seleccionado al carrito
-        carrito.push(producto);
+        // Agrega el producto seleccionado al carrito (guarda solo la información necesaria)
+        carrito.push({ nombre: producto.nombre, precio: producto.precio });
 
         // Guarda el carrito actualizado en localStorage asociado con la identificación del usuario
         localStorage.setItem(`carrito_${user.uid}`, JSON.stringify(carrito));
 
         // Actualiza el contador del carrito
         actualizarCarrito();
+
+        // Renderiza los productos en el carrito después de agregar uno
+        await obtenerProductosEnCarrito().then(productos => {
+            renderizarProductosEnCarrito(productos);
+        });
     } else {
         // El usuario no está autenticado, mostrar un mensaje o redirigir al login
         alert('Debes iniciar sesión para agregar productos al carrito.');
@@ -42,26 +47,28 @@ function actualizarCarrito() {
 
     if (user) {
         const contadorCarrito = document.getElementById('contador-carrito');
-        let contador = JSON.parse(localStorage.getItem(`carrito_${user.uid}`))?.length || 0;
-        contadorCarrito.textContent = contador;
+
+        if (contadorCarrito) {
+            let contador = JSON.parse(localStorage.getItem(`carrito_${user.uid}`))?.length || 0;
+            contadorCarrito.textContent = contador;
+        }
     }
 }
+
 // Define el tamaño deseado de la imagen
 const IMG_WIDTH = 20;
 const IMG_HEIGHT = 20;
 
-function renderizarProductos(productos) {
+// Función para renderizar los productos en la tienda
+function renderizarProductosEnTienda(productos) {
     const contenedorProductos = document.getElementById('contenedor-productos');
-    
-    // Limpiar los eventos clic existentes antes de renderizar los productos
-    contenedorProductos.innerHTML = '';
 
     productos.forEach(producto => {
         const card = document.createElement('div');
         card.className = 'col-md-4 mb-4';
 
         // Asegúrate de que la propiedad "imagen" exista en tu objeto de producto
-        const imageUrl = producto.imagen || ''; 
+        const imageUrl = producto.imagen || '';
 
         card.innerHTML = `
             <div class="card">
@@ -85,7 +92,7 @@ function renderizarProductos(productos) {
             boton.addEventListener('click', function () {
                 const nombre = boton.getAttribute('data-nombre');
                 const precio = parseFloat(boton.getAttribute('data-precio'));
-                const producto = { nombre, precio };
+                const producto = { nombre, precio, ...producto }; // Incluye todos los atributos del producto
                 agregarAlCarrito(producto);
             });
         });
@@ -95,12 +102,48 @@ function renderizarProductos(productos) {
     actualizarCarrito();
 }
 
-// Inicializa el carrito en localStorage si no existe
-if (!localStorage.getItem('carrito')) {
-    localStorage.setItem('carrito', JSON.stringify([]));
+// Función para renderizar los productos en el carrito
+function renderizarProductosEnCarrito(productos) {
+    const listaCarrito = document.getElementById('lista-carrito');
+
+    // Limpiar la lista antes de renderizar los productos
+    listaCarrito.innerHTML = '';
+
+    productos.forEach(producto => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${producto.nombre} - $${producto.precio.toFixed(2)}`;
+        listaCarrito.appendChild(listItem);
+    });
+
+    // Calcular y mostrar el total
+    const totalCarrito = document.getElementById('total-carrito');
+    const total = productos.reduce((acc, producto) => acc + producto.precio, 0);
+    totalCarrito.textContent = `$${total.toFixed(2)}`;
+}
+// Llama a la función para obtener los productos del carrito y luego renderizarlos
+obtenerProductosEnCarrito().then(productos => {
+    // Verificar si la página es carrito.html antes de renderizar los productos
+    if (window.location.pathname.includes('carrito.html')) {
+        renderizarProductosEnCarrito(productos);
+    }
+});
+
+// Función para obtener los productos del carrito desde localStorage
+async function obtenerProductosEnCarrito() {
+    // Verificar si el usuario está autenticado
+    const user = currentUser(auth);
+
+    if (user) {
+        // Recupera la lista de productos en el carrito desde localStorage (si existe)
+        return JSON.parse(localStorage.getItem(`carrito_${user.uid}`)) || [];
+    } else {
+        // Si el usuario no está autenticado, retorna un array vacío
+        return [];
+    }
 }
 
-// Llama a la función para obtener los productos y luego renderizarlos
+// Llama a la función para obtener los productos y luego renderizarlos en la tienda
 obtenerProductos().then(productos => {
-    renderizarProductos(productos);
+    renderizarProductosEnTienda(productos);
 });
+
